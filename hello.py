2d90b1fe002fe3f9a4f0c682c87d978e72800902
Willie2511 <60076968+Willie2511@users.noreply.github.com>
2020-02-14 21:16:10 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,4 @@
+n = input("Please Enter Name: ")
+print ("Hello", (n))
+
+
---
+++ sub.py
@@ -0,0 +1,4 @@
+x = int(input("Enter First Number: "))
+y = int(input("Enter Second Number: "))
+answer = x - y
+print ("{} minus {} is {} ".format(x, y, answer))
