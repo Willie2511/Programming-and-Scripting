25d5c0a1e8d8e771b39db0100bfeaac330c49d0b
Willie2511 <60076968+Willie2511@users.noreply.github.com>
2020-02-20 21:34:48 +0000

    Add files via upload

---
+++ squareroot.py
@@ -0,0 +1,10 @@
+# Willie Byrne
+# This program will take a positive floating 
+# point number as input and outputs an approx
+# square root.
+
+import math
+
+x = float(input("Please enter a number: "))
+print ("The square root of ",x,"is", math.sqrt(x))
+
